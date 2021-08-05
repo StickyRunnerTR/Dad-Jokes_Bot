3e21fa90f7f0414860f89109c3cdfa03b89c782a
StickyRunnerTR <85073813+StickyRunnerTR@users.noreply.github.com>
2021-08-05 18:05:02 +0300

    Created Dad-Jokes_Bot
    A basic bot which gives you a dad joke if you type im or i am or i'm

---
+++ Dad_Joke.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+
+
+client = commands.Bot(command_prefix=None, help_command=None)
+
+@client.event
+async def on_ready():
+    print('Currently Online as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+
+    if message.content.lower().startswith("i'm dad" or "im dad" or "i am dad"):
+        await message.channel.send('No, I am dad')
+
+    elif message.content.lower().startswith("i'm "):
+        await message.channel.send('Hi {}, i\'m Dad'.format(message.content[4:]))
+        await message.add_reaction('<:OMEGALUL:861981687542186045>')
+    
+    elif message.content.lower().startswith("im "):
+        await message.channel.send('Hi {}, i\'m Dad'.format(message.content[3:]))
+        await message.add_reaction('<:OMEGALUL:861981687542186045>')
+
+    elif message.content.lower().startswith("i am "):
+        await message.channel.send('Hi {}, i\'m Dad'.format(message.content[5:]))
+        await message.add_reaction('<:OMEGALUL:861981687542186045>')
+   
+
+client.run(os.getenv('TOKEN'))
